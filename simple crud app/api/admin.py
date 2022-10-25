13b2ee69c1f827628648d5d267b0f748a7b69ccb
chidananda <chidanandakm99@gmail.com>
2022-10-25 14:03:31 +0530

    changed admin site registeration

--- simple crud app/api/admin.py
+++ simple crud app/api/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from .models import Location, Item
 
 # Register your models here.
+# another way of registering to admin site
+@admin.register(Item)
+class ItemsList(admin.ModelAdmin):
+    list_display = ['id', 'itemName', 'itemLocation']
+@admin.register(Location)
+class ItemsList(admin.ModelAdmin):
+    list_display = ['id', 'locationName']
 
-admin.site.register(Location)
-admin.site.register(Item)
+# admin.site.register(Location)
+# admin.site.register(Item)
